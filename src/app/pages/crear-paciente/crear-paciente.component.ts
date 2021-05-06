10ea26d5a586c85d4b798694a1420c7c3143e6c4
eliasmg12345 <randyofgod666@gmail.com>
2021-05-06 12:31:23 -0400

    4

--- src/app/pages/crear-paciente/crear-paciente.component.ts
+++ src/app/pages/crear-paciente/crear-paciente.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PacientesService } from '../../services/pacientes.service';
+import Swal from 'sweetalert2/src/sweetalert2.js';
 
 @Component({
   selector: 'app-crear-paciente',
@@ -16,12 +17,54 @@ export class CrearPacienteComponent {
    }
 
    AltaPaciente(){
+
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+    
+    swalWithBootstrapButtons.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, cancel!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    })
+
+
+
+    
+/*
      this.pacientesServices.altaPaciente(this.pacientes).subscribe(resp=>{
        if(resp['resultado']==='OK'){
          console.log('exitoso');
          this.router.navigate(['nuevo-historial']);
        }
      });
+
+*/
    }
 
 }
