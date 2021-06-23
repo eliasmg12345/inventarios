21b2df473ae053961a03c51058c625408f19cd7d
eliasmg12345 <randyofgod666@gmail.com>
2021-06-23 10:31:46 -0400

    -

--- src/app/login.guard.ts
+++ src/app/login.guard.ts
@@ -1,15 +1,26 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Injectable, Input } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { LoginServiceService } from './services/login-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuard implements CanActivate {
+
+
+  constructor(private loginService: LoginServiceService, private router:Router){}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+      if(!this.loginService.loginUsuario){
+        this.router.navigate(['/']);
+        return true;
+      }
+      return true;
+    
   }
   
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -34,7 +34,7 @@ export class LoginComponent implements OnInit {
         console.log(datos['mensaje']);
         console.log('sd');
       }
-    })
+    });
 
   }
 
